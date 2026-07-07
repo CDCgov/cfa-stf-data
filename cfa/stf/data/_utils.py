5e62c847fe1e433023b279b35e65638b52339a5a
Damon Bayer <xum8@cdc.gov>
2026-07-07 17:41:25 -0500

    fix type hints

--- cfa/stf/data/_utils.py
+++ cfa/stf/data/_utils.py
@@ -3,14 +3,14 @@ from typing import overload
 
 
 @overload
-def ensure_list[T](x: None) -> None: ...
+def ensure_list(x: None) -> list[None]: ...
 
 
 @overload
 def ensure_list[T](x: T | Iterable[T]) -> list[T]: ...
 
 
-def ensure_list[T](x: T | Iterable[T] | None) -> list[T] | None:
+def ensure_list[T](x: T | Iterable[T] | None) -> list[T] | list[None]:
     if x is None:
         return []
     elif isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
